2be36ebeefb1201c3e63cc655fbc3cdb4f215801
Ametz Mendieta <a_mendieta@fpzornotza.com>
2022-03-02 12:02:50 +0100

    Azterketa

---
+++ DibujarPoligono/src/base/PoligonoTest.java
@@ -0,0 +1,20 @@
+package base;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PoligonoTest {
+
+	@Test
+	void test() {
+		Poligono datos = new Poligono("Hexagono");
+		int lados = 6;
+		double longitudLado = 10;
+		datos.setLados(lados);
+		datos.setLongitudLado(longitudLado);
+		double completo = datos.calcularPerimetro();
+		assertEquals(60, completo);	
+		}
+	
+}
